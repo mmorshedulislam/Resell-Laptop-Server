20b3c9dafd6712465d7fb2d6af22b6afd01eeaca
MORSHED-HOME <morshed952640@gmail.com>
2022-11-30 23:10:20 +0600

    Some code changes and fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -39,11 +39,22 @@ const verifyEmail = (req, res, next) => {
 // MONGODB STARTS
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wobl1ex.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1,
+// });
+
+const client = new MongoClient(
+  uri,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  },
+  { connectTimeoutMS: 30000 },
+  { keepAlive: 1 }
+);
 
 async function run() {
   try {
